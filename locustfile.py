c85c48606e0b69db5dd2e367740e7486a3a76e4e
AshrafIb <54529492+AshrafIb@users.noreply.github.com>
2021-01-08 05:55:56 +0100

    locust

--- locustfile.py
+++ locustfile.py
@@ -7,8 +7,26 @@ class CheckSite(HttpUser):
         self.client.get("/")
         
     @task
-    def get_oredict(self):
-        self.client.post("/predict")
+    def get_predict(self):
+        self.client.post("/predict",json={
+   "CHAS":{
+      "0":0
+   },
+   "RM":{
+      "0":6.575
+   },
+   "TAX":{
+      "0":296.0
+   },
+   "PTRATIO":{
+      "0":15.3
+   },
+   "B":{
+      "0":396.9
+   },
+   "LSTAT":{
+      "0":4.98
+   })
 
 class Locusttest(HttpUser):
     task_set =CheckSite
